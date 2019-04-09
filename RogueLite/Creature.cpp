b768342c5c0b5c6e0fd76b3558c0c4505063e997
Nick Truong <nicktruong@outlook.com>
2019-04-09 19:34:12 -0400

    a

---
+++ RogueLite/Creature.cpp
@@ -0,0 +1,15 @@
+#include "Creature.h"
+
+namespace creature
+{
+    void move(Creature c, float dt)
+    {
+        c.velocity = glm::normalize(c.velocity);
+        c.position += c.velocity * c.speed * dt;
+    }
+
+    void increment_velocity_up(Creature c) { c.velocity.y++; }
+    void increment_velocity_down(Creature c) { c.velocity.y--; }
+    void increment_velocity_left(Creature c) { c.velocity.x--; }
+    void increment_velocity_right(Creature c) { c.velocity.x++; }
+}  // namespace physics
