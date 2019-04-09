123b77118ea0c7ae02bb8f63973c91d11ccf6030
Maximillian Rink <devcallidus@gmail.com>
2019-04-09 14:14:40 -0400

    Removing Fragment Shader debugging option

--- RogueLite/FragmentShader.glsl
+++ RogueLite/FragmentShader.glsl
@@ -9,8 +9,8 @@ uniform sampler2D image;
 void main()
 {    
     // gl_InstanceID
-    //color = vec4(fSpriteColor, 1.0) * texture(image, TexCoords);
+    color = vec4(fSpriteColor, 1.0) * texture(image, TexCoords);
 	//color = texture(image, TexCoords);
-	color = vec4(TexCoords, 0.2f, 1.0f);
+	//color = vec4(TexCoords, 0.2f, 1.0f);
 	//color = vec4(image, 0.0f, 0.0f, 1.0f);
 }  
